4250cfe236620ffc7212c92464faa45a1aac7c7d
Uri Arev <uri.arev@gmail.com>
2020-12-16 15:00:38 +0200

    [minor] Adding /About route to the server

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(__dirname + "/public"))
+
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname + "/public/main.html"));
+});
+
+app.get("/about", (req, res) => {
+	res.sendFile(path.join(__dirname + "/public/about.html"));
 });
 
 const PORT = 80;
